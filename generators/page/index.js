c479ffd31f875e8cf1137f8b9ad549e5d2089db4
jeantimex <jean.timex@gmail.com>
2020-02-20 10:08:54 -0800

    Complete page sub generator

--- generators/page/index.js
+++ generators/page/index.js
@@ -5,10 +5,44 @@ module.exports = class extends Generator {
   constructor(args, opts) {
     super(args, opts);
 
-    this.argument('pagename', { type: String, required: true });
+    this.argument('name', { type: String, required: true });
+  }
+
+  prompting() {
+    const prompts = [
+      {
+        type: 'confirm',
+        name: 'typescript',
+        message: 'Use TypeScript?',
+        default: false,
+      },
+    ];
+    return this.prompt(prompts).then(props => {
+      // To access props later use this.props.someAnswer;
+      this.props = props;
+    });
   }
 
   writing() {
-    this.log(this.options.pagename);
+    const name = this.options.name
+      .toLocaleLowerCase()
+      .split(' ')
+      .join('-');
+
+    if (
+      this.fs.exists(this.destinationPath('src', 'pages', name + '.js')) ||
+      this.fs.exists(this.destinationPath('src', 'pages', name + '.ts'))
+    ) {
+      throw new Error('page `' + name + '` exists');
+    }
+
+    const pageFilePath = this.destinationPath(
+      'src',
+      'pages',
+      name + (this.props.typescript ? '.ts' : '.js')
+    );
+
+    // Create a new file.
+    this.fs.write(pageFilePath, '');
   }
 };
